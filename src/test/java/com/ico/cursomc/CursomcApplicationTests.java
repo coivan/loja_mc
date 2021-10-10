a837103391c5166a0f1622379ce8d748c6e74ac6
Ivan Cardoso <cardoso.ivan2012@gmail.com>
2021-10-10 15:09:02 -0300

    Projeto criado

---
+++ src/test/java/com/ico/cursomc/CursomcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ico.cursomc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursomcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
